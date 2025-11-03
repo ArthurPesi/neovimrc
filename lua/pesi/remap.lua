1f29592ba055bea46e7465f1b0646b54966cf4bd
Pesi <arthur.pesi@gmail.com>
2025-11-03 10:34:59 -0300

    .

--- lua/pesi/remap.lua
+++ lua/pesi/remap.lua
@@ -1,8 +1,5 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
 vim.keymap.set("n", "<C-d>", "<c-d>zz")
 vim.keymap.set("n", "<C-u>", "<c-u>zz")
 
@@ -11,16 +8,10 @@ vim.keymap.set("n", "<leader>/", "_i// <Esc>")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
 vim.keymap.set({"n", "x"}, "<leader>P", [["*p]])
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 -- undotree
 vim.keymap.set("n", "<leader>t", vim.cmd.UndotreeToggle)
